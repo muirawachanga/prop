12c2a490bf987cd60cb1db950b7e1c9a1513e88b
Alex Ng'ang'a <lxnganga@gmail.com>
2016-08-27 15:42:05 +0100

    Show arrears on invoice as a message

--- property/property_management/hooks/doc_hooks.py
+++ property/property_management/hooks/doc_hooks.py
@@ -1,11 +1,23 @@
 from __future__ import unicode_literals
 
 import frappe
+from frappe.utils import flt, fmt_money, today
 
 
 def sales_invoice_arrears(doc, event):
-    arrears = frappe.db.sql("select sum(outstanding_amount) from `tabSales Invoice` where customer = %s "
-                            "and docstatus = 1 and name <> %s;", (doc.customer, doc.name or ''))[0][0]
-    if arrears is None:
+    # Don't do this for returns
+    if doc.is_return == 1:
         return
-    doc.outstanding_arrears = arrears
+    # Do this only for rental related invoices.
+    if doc.tenancy_contract in (None, ''):
+        return
+    arrears = frappe.db.sql("select sum(outstanding_amount) from `tabSales Invoice` where customer = %s "
+                            "and docstatus = 1 and due_date < %s and tenancy_contract = %s and is_return != 1 "
+                            "and outstanding_amount > 0",
+                            (doc.customer, today(), doc.tenancy_contract))[0][0]
+    if arrears is None or arrears <= 0:
+        doc.arrears_note = ""
+        return
+    doc.arrears_note = "You have {0} in pending arrears, your total amount to pay is: {1}" \
+        .format(fmt_money(arrears, 2, doc.currency), fmt_money(flt(doc.outstanding_amount + arrears),
+                                                               2, doc.currency))
